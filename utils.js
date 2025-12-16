ad4611e25ef87a5b9de6bef388bbd2b753df15e4
The Eagle <theeaglebenson@gmail.com>
2025-12-16 11:35:14 +0000

    Add toast notification and update toast function in utils.js

--- utils.js
+++ utils.js
@@ -11,18 +11,22 @@ function errorCallback(err){
     switch (err.code) {
         case err.UNKNOWN_ERROR:
             errMessage = 'An unknown error occurred';
+            showToast(errMessage, 'error');
             console.log(`${err.code} — ${err.message}`)
             break;
         case err.PERMISSION_DENIED:
             errMessage = 'You denied Eagle Eye to retrieve your location';
+            showToast(errMessage, 'error');
             console.log(`${err.code} — ${err.message}`)
             break;
         case err.POSITION_UNAVAILABLE:
             errMessage = 'Unable to retrieve your location. Refresh the page and try again';
+            showToast(errMessage, 'error');
             console.log(`${err.code} — ${err.message}`)
             break;
         case err.TIMEOUT:
             errMessage = 'Location retrieval service timeout. Refresh and try again';
+            showToast(errMessage, 'error');
             console.log(`${err.code} — ${err.message}`)
             break;
     }
@@ -40,6 +44,7 @@ function showToast(message = 'Hello World!', type = 'info'){
                             background: type === 'error' ? "#ef4444" : "#0ea5e9",
                             display: "flex",
                             alignItems: "center",
+                            justifySelf: "center",
                             gap: "10px",
                             borderRadius: "10px",
                             boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1)",
